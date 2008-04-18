4092d99ea389c5d72c0afadacf27c52a5d81c62d
Marc Berthold <ber@sastst.hq.k.grp>
2008-04-18 15:42:27 +0200

    Added the tests for the smtp auth preferences

--- spec/controllers/admin/preferences_controller_spec.rb
+++ spec/controllers/admin/preferences_controller_spec.rb
@@ -60,10 +60,85 @@ describe Admin::PreferencesController do
                                      :email_notifications => "1" }
       flash[:error].should_not be_nil
     end
+	
+	it "should render messages for smtp auth error" do
+      put :update, :preferences => { :smtp_server => "smtp.example.com", :domain => "example.com", 
+                                     :email_notifications => "1", :smtp_auth => "1", :smtp_server_username => "bozo", :smtp_server_password => ""}
+      response.body.should =~ /errorExplanation/
+    end
   
     it "should update the analytics attribute" do
       put :update, :preferences => { :analytics => "Google analytics" }
       @prefs.reload.analytics.should == "Google analytics"
     end
+
+	it "should render messages for smtp auth error" do
+      put :update, :preferences => { :smtp_server => "smtp.example.com", :domain => "example.com", 
+                                     :email_notifications => "1", :smtp_auth => "1", :smtp_server_username => "", :smtp_server_password => "password"}
+      response.body.should =~ /errorExplanation/
+    end
+	it "should render messages for smtp auth error" do
+      put :update, :preferences => { :smtp_server => "smtp.example.com", :domain => "example.com", 
+                                     :email_notifications => "1", :smtp_auth => "1", :smtp_server_username => "", :smtp_server_password => ""}
+      response.body.should =~ /errorExplanation/
+    end
+	it "should render messages for smtp auth error" do
+      put :update, :preferences => { :smtp_server => "", :domain => "", 
+                                     :email_notifications => "", :smtp_auth => "1", :smtp_server_username => "bozzo", :smtp_server_password => "password"}
+      response.body.should =~ /errorExplanation/
+    end
+
+    it "should update smtp auth" do
+      @prefs.smtp_auth = false
+      @prefs.save!
+      @prefs.smtp_auth.should be_false
+      put :update, :preferences => { :smtp_server => "smtp.example.com",
+                                     :domain => "example.com", 
+                                     :email_notifications => "1",
+									 :smtp_auth =>  "1",
+									 :smtp_server_username => "username",
+									 :smtp_server_password => "password"
+									 }
+      @prefs.reload.smtp_auth.should be_true
+    end
+
+	it "should update smtp password" do
+      @prefs.smtp_auth = false
+      @prefs.smtp_server_username = false
+      @prefs.smtp_server_password = false
+      @prefs.save!
+      @prefs.smtp_server_password.should be_false
+      put :update, :preferences => { :smtp_server => "smtp.example.com",
+                                     :domain => "example.com", 
+                                     :email_notifications => "1",
+									 :smtp_auth =>  "1",
+									 :smtp_server_username => "username",
+									 :smtp_server_password => "password"
+									 }
+      @prefs.reload.smtp_server_password.should == "password"
+    end
+	
+    it "should have a flash warning if the smtp auth username changes" do
+      put :update, :preferences => { :smtp_server => @prefs.smtp_server,
+                                     :domain => @prefs.domain, 
+                                     :email_notifications => @prefs.email_notifications,
+									 :smtp_auth =>  @prefs.smtp_auth,
+									 :smtp_server_username => "newUsername",
+									 :smtp_server_password => @prefs.smtp_server_password
+									 }
+      flash[:error].should_not be_nil
+    end
+
+    it "should have a flash warning if the smtp auth password changes" do
+      put :update, :preferences => { :smtp_server => @prefs.smtp_server,
+                                     :domain => @prefs.domain, 
+                                     :email_notifications => @prefs.email_notifications,
+									 :smtp_auth =>  @prefs.smtp_auth,
+									 :smtp_server_username => @prefs.smtp_server_username,
+									 :smtp_server_password => "newPassword"
+									 }
+      flash[:error].should_not be_nil
+    end
+
   end
 end
