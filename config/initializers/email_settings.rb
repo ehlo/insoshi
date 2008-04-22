872bfbe93440d89995c84478475c40a970d7bcb9
Marc Berthold <ber@sastst.hq.k.grp>
2008-04-22 08:16:18 +0200

    Deleted preference.rb and corrected email_settings.rb so that smtp auth config from teh DB works.

--- config/initializers/email_settings.rb
+++ config/initializers/email_settings.rb
@@ -3,11 +3,22 @@ begin
     global_prefs = Preference.find(:first)
     if global_prefs.email_notifications?
       ActionMailer::Base.delivery_method = :smtp
-      ActionMailer::Base.smtp_settings = {
-        :address    => global_prefs.smtp_server,
-        :port       => 25,
-        :domain     => global_prefs.domain
-      }
+		if global_prefs.smtp_auth?
+		  ActionMailer::Base.smtp_settings = {
+			:address    => global_prefs.smtp_server,
+			:port       => 25,
+			:domain     => global_prefs.domain,
+			:user_name => global_prefs.smtp_server_username,
+			:password => global_prefs.smtp_server_password,
+			:authentication => :login
+		  }
+		else
+		  ActionMailer::Base.smtp_settings = {
+			:address    => global_prefs.smtp_server,
+			:port       => 25,
+			:domain     => global_prefs.domain
+		  }
+		end
     end
   end
 rescue
