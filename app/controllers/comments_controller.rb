b3b4ccc2755e0310a19795357963ce2f0bc7a69c
ber@sastst.hq.k.grp <ber@sastst.hq.k.grp>
2008-05-08 11:43:19 +0200

    Add sender ip to the comments.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -19,7 +19,7 @@ class CommentsController < ApplicationController
   # Used for both wall and blog comments.
   def create
     @comment = parent.comments.new(params[:comment].
-                                   merge(:commenter => current_person))
+                                   merge(:commenter => current_person, :sender_ip => request.env_table["REMOTE_ADDR"]))
     
     respond_to do |format|
       if @comment.save
--- db/migrate/021_add_sender_ip_communications.rb
+++ db/migrate/021_add_sender_ip_communications.rb
@@ -1,9 +1,11 @@
 class AddSenderIpCommunications < ActiveRecord::Migration
   def self.up
     add_column :communications, :sender_ip, :string
+    add_column :comments, :sender_ip, :string
   end
 
   def self.down
-    remove_column :communications, :sender_ip, :string
+    remove_column :communications, :sender_ip
+    remove_column :comments, :sender_ip
   end
 end
